31baae0ab439eb3724a25a5e4a1fe83b4ee1c2e7
Sirobchik <bvzorex1@gmail.com>
2026-05-08 19:58:14 +0500

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -4,23 +4,40 @@ package com.example.shtrih2
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts.GetContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
 import com.google.zxing.integration.android.IntentIntegrator
 import org.apache.poi.ss.usermodel.Workbook
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import com.example.shtrih2.R
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
 
 class MainActivity : AppCompatActivity() {
-    private val database = HashMap<String?, String?>()
+    private val viewModel by viewModels<Swag>()
+    private val database by lazy {viewModel.database}
     private var tvResult: TextView? = null
 
     var filePicker: ActivityResultLauncher<String?>? = null
+    private val scaner=registerForActivityResult(ScanContract()){
+        if (it.getContents() != null) {
+            val code = it.getContents()
+            val product = database.get(code)
+
+            tvResult!!.setText(code + " → " + product)
+//            tvResult!!.setText("Excel загружен: " + database.size + " записей")
+        } else {
+            tvResult!!.setText("Сканирование отменено")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,15 +73,24 @@ class MainActivity : AppCompatActivity() {
             database.clear()
 
             for (row in sheet) {
-                val barcodeCell = row.getCell(0)
-                val nameCell = row.getCell(1)
+                val barcodeCell = row.getCell(2)
+                val nameCell = row.getCell(0)
+                try {
+                    if (barcodeCell != null && nameCell != null) {
+                        val barcode = barcodeCell.stringCellValue.trim()
+                        val name = nameCell.numericCellValue.toString()
+                        Log.e("System.err","${barcode} ${name}")
+                        database.put(barcode, name)
+                    }
 
-                if (barcodeCell != null && nameCell != null) {
-                    val barcode = barcodeCell.toString()
-                    val name = nameCell.toString()
-                    database.put(barcode, name)
+                }catch (e: Exception){}
+
+//                if (barcodeCell != null && nameCell != null) {
+//                    val barcode = barcodeCell.richStringCellValue.string.trim()
+//                    val name = nameCell.stringCellValue
+//                    database.put(barcode, name)
                 }
-            }
+
 
             tvResult!!.setText("Excel загружен: " + database.size + " записей")
         } catch (e: Exception) {
@@ -74,10 +100,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startScan() {
-        val integrator = IntentIntegrator(this)
-        integrator.setPrompt("Сканируйте штрих-код")
-        integrator.setBeepEnabled(true)
-        integrator.initiateScan()
+        scaner.launch(ScanOptions())
+//        val integrator = IntentIntegrator(this)
+//        integrator.setDesiredBarcodeFormats(ScanOptions.CODE_128)
+//        integrator.setPrompt("Сканируйте штрих-код")
+//        integrator.setBeepEnabled(true)
+//        integrator.initiateScan()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -97,3 +125,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+class Swag: ViewModel(){
+    val database = HashMap<String?, String?>()
